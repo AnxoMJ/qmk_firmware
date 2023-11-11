f7261a283f65583a0f39460b7c2ad67973ae5d2e
AnxoMJ <anxo.mazas@gmail.com>
2023-11-11 20:09:59 +0100

    debug

--- keyboards/feker/ik75/keymaps/amj/keymap.c
+++ keyboards/feker/ik75/keymaps/amj/keymap.c
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 */
     /*  Row:    0        1        2        3      4      5      6       7      8      9        10       11       12       13       14       15     */
     [_BASE] = LAYOUT(
-                KC_ESC,  KC_F1,   KC_F2,   KC_F3, KC_F4, KC_F5, KC_F6,  KC_F7, KC_F8, KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,           KC_MUTE,
+                KC_ESC,  KC_F1,   KC_F2,   KC_F3, KC_F4, KC_F5, KC_F6,  KC_F7, KC_F8, KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,           LT(0,KC_MUTE),
                 KC_GRV,  KC_1,    KC_2,    KC_3,  KC_4,  KC_5,  KC_6,   KC_7,  KC_8,  KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_INS,
                 KC_TAB,  KC_Q,    KC_W,    KC_E,  KC_R,  KC_T,  KC_Y,   KC_U,  KC_I,  KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_END,
                 KC_CAPS, KC_A,    KC_S,    KC_D,  KC_F,  KC_G,  KC_H,   KC_J,  KC_K,  KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,
@@ -143,9 +143,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             rgb_matrix_set_color_all(0, 0, 0);
             return false;
-        case MO(_FN):{ //checks if fn key is pressed
-            fn_pressed = !(fn_pressed);
-        }
+        //case MO(_FN):{ //checks if fn key is pressed
+        //    fn_pressed = !(fn_pressed);
+       // }
         case KC_MUTE:{ //checks if fn key is pressed
             if (record->event.pressed) {
                 mute_pressed = !(mute_pressed);
@@ -216,6 +216,6 @@ bool rgb_matrix_indicators_user(void) {
 #ifdef ENCODER_MAP_ENABLE
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [_FN]   = { ENCODER_CCW_CW(RGB_SAD, RGB_SAI) },
+    [_FN]   = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
 };
 #endif
